9127d2fc52773552e86068e6fe8adc6368eb0f59
Fergal <fergal.mccarthy@gmail.com>
2024-12-09 16:29:26 +0000

    Add requirements.sh for build process

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 pip install Flask
 pip install git+https://$UNDERSTAT_PAT@github.com/hmallen/understatapi.git
+=======
+pip install Flask
+pip install git+https://$UNDERSTAT_PAT@github.com/hmallen/understatapi.git
+>>>>>>> 5d4c714 (Add requirements.sh for build process)
